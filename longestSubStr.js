3e294b0f3a0c2b640e4d69d00ac137fa492627bf
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2021-03-05 11:43:45 +0000

    solve longestSubstring problem

---
+++ longestSubStr.js
@@ -0,0 +1,18 @@
+const lengthOfLongestSubstring = s => {
+  let maxLen = 0,
+      arr = [];  
+  for (i = 0; i < s.length; i++) {
+    if (arr.length > maxLen) {
+      maxLen = arr.length;
+    }
+    if (arr.includes(s[i])) {
+      while (arr.includes(s[i])) {
+        arr.shift();
+      }
+    }
+    arr.push(s[i])
+  }
+  return Math.max(maxLen, arr.length)
+}
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
